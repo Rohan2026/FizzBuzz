66c3f2fcc33e02a077b665e390307b1d8824c8ff
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-10-19 11:44:01 -0700

    Fizzbuzz progress

--- Fizzbuzz/FizzBuzz.java
+++ Fizzbuzz/FizzBuzz.java
@@ -3,7 +3,16 @@ public class FizzBuzz
 {
     public static void main(String[] args) 
     {
-        System.out.println("FizzBuzz");
+        int count = 1;
+        while (count < 101) 
+        {
+            if (count == 0 && count%3 == 0) 
+            {
+                System.out.println("FizzBuzz");
+            } 
+            else System.out.println(count);
+            count = count + 1;
+        }  
     }
 }
 
