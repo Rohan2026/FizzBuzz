ddbc51bf118f5c7e5cfb6a10d112b86ecbbdf541
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-10-20 10:50:48 -0700

    FizzBuzz Complete

--- Fizzbuzz/FizzBuzz.java
+++ Fizzbuzz/FizzBuzz.java
@@ -6,11 +6,22 @@ public class FizzBuzz
         int count = 1;
         while (count < 101) 
         {
-            if (count == 0 && count%3 == 0) 
+            if (count/15 * 15 == count)
             {
                 System.out.println("FizzBuzz");
             } 
-            else System.out.println(count);
+            else if (count/5 * 5 == count)
+            {
+                System.out.println("Buzz");
+            }
+            else if (count/3 * 3 == count)
+            {
+                System.out.println("Fizz");
+            }
+            else 
+            {
+                System.out.println(count);
+            }
             count = count + 1;
         }  
     }
